078d300141c895b93f25930459a50a6f0a7955fd
Shreyas Katalkar <katalkarshreyas@gmail.com>
2024-03-30 13:51:07 +0530

    changes 4

--- src/components/sub-components/myProject-inside.js
+++ src/components/sub-components/myProject-inside.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -30,10 +30,6 @@ const myProjects = [
 ]
 
 function MyProjectInside() {
-    const [isClicked , setIsClicked] = useState(false);
-    function ShowMore() {
-        setIsClicked(true);
-    }
 
     
 
