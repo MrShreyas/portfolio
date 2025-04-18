9fccb878db7fc93be14815c21c9b9799c7b6e219
Shreyas Katalkar <katalkarshreyas@gmail.com>
2025-04-19 00:54:11 +0530

    check changes

--- components/skills.tsx
+++ components/skills.tsx
@@ -73,7 +73,7 @@ export function Skills() {
       technologies: ["Git", "GitHub",],
     },
   ]
-
+  //skills section
   return (
     <section id="skills" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
